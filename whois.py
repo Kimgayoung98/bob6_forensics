fc75855201e35137860d7d813f25d5229541c5a3
Kimgayoung98 <rkdud0520@naver.com>
2017-07-16 18:11:54 +0900

    more simple code
    more simple whois

--- whois.py
+++ whois.py
@@ -1,32 +1,28 @@
 import pythonwhois
 import json
-import sys
+import sys #sys.argv
 import datetime
 import yaml
 
-def datetime_handler(x):
-    if isinstance(x, datetime.datetime):
-        return x.isoformat()
-    raise TypeError("Unknown type")
+def datetime_control(x):
+    if isinstance(x, datetime.datetime): #isinstance type == datetime.datetime -> return x.isoformat()
+        return x.isoformat() # HH:MM:SS
 
-filename = sys.argv[0]
-if len(sys.argv) is 1:
-	exit('Usage: ' + filename + ' filename')
-domainlist = sys.argv[1]
+domainlist = sys.argv[1] #input domainlist
 
-f = open('url_sample.txt', 'r')
-while True:
-	line = f.readline()
-	if len(line) == 0:
+f = open(domainlist, 'r') # file open to read
+while True: # loop
+	line = f.readline() # read one line
+	if len(line) == 0: #length of line == 0
 		break
-	print line
-	whoiss = pythonwhois.get_whois(line)
-	print whoiss
-	my_json = json.dumps(whoiss, default = datetime_handler, indent = 4)
-	d = yaml.load(my_json)
+	whoiss = pythonwhois.get_whois(line) #whois information of one line
+	my_json = json.dumps(whoiss, default = datetime_control, indent = 5) 
+	#switch whoiss into json type and indent 4 and include datetime_handler function
+	swichdic = yaml.load(my_json)
+	#switch my_json to format yaml(json) 
 
-	with open('whois.json', 'w')as make_file:
-		json.dump(d,make_file)
-		print d
+	with open('whois.json', 'w')as make_file: #make file whois.json and write, if end task, close make_file 
+ 		json.dump(swichdic, make_file) # switch swichdic into json type.
+		print swichdic #print swichdic
 
-f.close()
+f.close() #file close
